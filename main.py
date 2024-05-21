1d857a1009dfb015ead1fec66588bd4d5f6fa82a
Alessandro <alessandrosoaresrangel1@gmail.com>
2024-05-21 16:10:45 -0300

    create a gitignore and set up the canvas

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+from tkinter import *
+# PASSWORD GENERATOR
 
-# ---------------------------- SAVE PASSWORD ------------------------------- #
+# SAVE PASSWORD
 
-# ---------------------------- UI SETUP ------------------------------- #
+# UI SETUP
+window = Tk()
+window.title("Password Manager")
+
+window.config(padx=20, pady=20)
+
+canvas = Canvas(width=200, height=200, highlightthickness=0)
+logo = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo)
+canvas.pack()
+
+window.mainloop()
