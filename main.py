85fbe3cba19dec56845e76e60f4d00a8f0650240
Alessandro <alessandrosoaresrangel1@gmail.com>
2024-05-21 16:46:49 -0300

    create the grid layout

--- main.py
+++ main.py
@@ -7,11 +7,35 @@ from tkinter import *
 window = Tk()
 window.title("Password Manager")
 
-window.config(padx=20, pady=20)
+window.config(padx=50, pady=50)
 
 canvas = Canvas(width=200, height=200, highlightthickness=0)
 logo = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo)
-canvas.pack()
+canvas.grid(column=1, row=0)
+
+website_label = Label(text="Website:")
+website_label.grid(column=0, row=1)
+
+website_entry = Entry()
+website_entry.grid(column=1, row=1, columnspan=2, sticky="EW")
+
+username_label = Label(text="Email/Nome de usuario:")
+username_label.grid(column=0, row=2)
+
+username_entry = Entry()
+username_entry.grid(column=1, row=2, columnspan=2, sticky="EW")
+
+password_label = Label(text="Senha:")
+password_label.grid(column=0, row=3)
+
+password_entry = Entry()
+password_entry.grid(column=1, row=3, sticky="EW")
+
+generate_button = Button(text="Gerar Senha")
+generate_button.grid(column=2, row=3, sticky="EW")
+
+add_button = Button(text="Adicionar")
+add_button.grid(column=1, row=4, columnspan=2, sticky="EW")
 
 window.mainloop()
