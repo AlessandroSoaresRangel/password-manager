c3a4585cc959b07c0a1d7c131edd950302ffe6c5
Alessandro <alessandrosoaresrangel1@gmail.com>
2024-05-24 12:03:05 -0300

    data store and message box

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
 from tkinter import *
+from tkinter import messagebox
 # PASSWORD GENERATOR
 
 # SAVE PASSWORD
+
+
+def save():
+    website = website_entry.get()
+    username = username_entry.get()
+    password = password_entry.get()
+
+    if not website or not username or not password:
+        messagebox.showinfo(title="Erro", message="Não pode haver campos vazios")
+    else:
+        is_ok = messagebox.askokcancel(title=website, message=f"Esses são os dados detalhados: \n"
+                                                              f"Email: {username} \n"
+                                                              f"Password: {password}\n"
+                                                              f"salvar?")
+        if is_ok:
+            with open("data.txt", "a") as file:
+                file.write(f"site: {website} | email/Nome de usuario: {username}"
+                           f" | Senha: {password} \n")
+
+    website_entry.delete(0, END)
+    password_entry.delete(0, END)
+
 
 # UI SETUP
 window = Tk()
@@ -19,6 +42,7 @@ website_label.grid(column=0, row=1)
 
 website_entry = Entry()
 website_entry.grid(column=1, row=1, columnspan=2, sticky="EW")
+website_entry.focus()
 
 username_label = Label(text="Email/Nome de usuario:")
 username_label.grid(column=0, row=2)
@@ -35,7 +59,7 @@ password_entry.grid(column=1, row=3, sticky="EW")
 generate_button = Button(text="Gerar Senha")
 generate_button.grid(column=2, row=3, sticky="EW")
 
-add_button = Button(text="Adicionar")
+add_button = Button(text="Adicionar", command=save)
 add_button.grid(column=1, row=4, columnspan=2, sticky="EW")
 
 window.mainloop()
